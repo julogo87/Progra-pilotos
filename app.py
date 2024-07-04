f8118baba31b879a5c4dbe28b3d8ac397d786163
julogo87 <julogo87@hotmail.com>
2024-07-04 16:34:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ def draw_text(ax, text, x, y, duration, align='center', **kwargs):
         truncated_text = text[:int(max_width * 50)] + '...'  # Ajustar longitud del texto basado en duración
         ax.text(x + duration / 2, y, truncated_text, ha=align, va='center', **kwargs)
 
-def generate_plot(df, additional_text, start_time, end_time, figsize=(11, 8.5)):
+def generate_plot(df, additional_text, start_time, end_time):
     order = ['N330QT', 'N331QT', 'N332QT', 'N334QT', 'N335QT', 'N336QT', 'N337QT']
     df['aeronave'] = pd.Categorical(df['Reg.'], categories=order, ordered=True)
     df = df.sort_values('aeronave', ascending=False)
-    fig, ax = plt.subplots(figsize=figsize)  # Tamaño especificado
+    fig, ax = plt.subplots(figsize=(11, 8.5))  # Tamaño carta horizontal
 
     for i, aeronave in enumerate(reversed(order)):
         vuelos_aeronave = df[df['aeronave'] == aeronave]
@@ -50,8 +50,8 @@ def generate_plot(df, additional_text, start_time, end_time, figsize=(11, 8.5)):
             
             draw_text(ax, vuelo['Flight'], start, i, duration, color='black', fontsize=8)
             draw_text(ax, vuelo['Trip'], start, i + 0.3, duration, color='blue', fontsize=8)  # Bajado ligeramente
-            draw_text(ax, vuelo['Notas'], start, i - 0.3, duration, color='green', fontsize=8)  # Bajado ligeramente
-            draw_text(ax, vuelo['Tripadi'], start, i - 0.5, duration, color='purple', fontsize=8)  # Bajado ligeramente
+            draw_text(ax, vuelo['Notas'], start, i - 0.25, duration, color='green', fontsize=8)
+            draw_text(ax, vuelo['Tripadi'], start, i - 0.4, duration, color='purple', fontsize=8)  # Subido ligeramente
 
             if text_fits(ax, vuelo['From'], start, duration):
                 ax.text(start, i + 0.2, vuelo['From'], ha='left', va='center', color='black', fontsize=8)
@@ -109,45 +109,32 @@ def process_and_plot(df, additional_text):
 
     return pdf_buffers, None
 
-def generate_full_plot(df, additional_text):
-    start_time = df['fecha_salida'].min().normalize() + pd.Timedelta(hours=5)
-    end_time = df['fecha_llegada'].max()
-    return generate_plot(df, additional_text, start_time, end_time, figsize=(31.5, 8.5))  # 80cm x 22cm in inches
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         table_data = request.form['table_data']
         additional_text = request.form.get('additional_text')
-        full_plot = request.form.get('full_plot', 'false') == 'true'
         try:
             df = pd.read_json(table_data)
-            df.rename(columns={'STD': 'fecha_salida', 'STA': 'fecha_llegada'}, inplace=True)
         except ValueError as e:
             return jsonify({'error': f"JSON parsing error: {e}"}), 400
 
-        if full_plot:
-            pdf_buffer = generate_full_plot(df, additional_text)
-            output = io.BytesIO()
-            output.write(pdf_buffer.getbuffer())
-            output.seek(0)
-            return send_file(output, as_attachment=True, download_name='programacion_vuelos_qt_full.pdf', mimetype='application/pdf')
-        else:
-            pdf_buffers, error = process_and_plot(df, additional_text)
-            if error:
-                return jsonify({'error': error}), 400
-            
-            output = io.BytesIO()
-            merger = PdfMerger()
-            for buf in pdf_buffers:
-                merger.append(buf)
-            merger.write(output)
-            merger.close()
-            output.seek(0)
-            
-            return send_file(output, as_attachment=True, download_name='programacion_vuelos_qt.pdf', mimetype='application/pdf')
+        pdf_buffers, error = process_and_plot(df, additional_text)
+        if error:
+            return jsonify({'error': error}), 400
+        
+        output = io.BytesIO()
+        merger = PdfMerger()
+        for buf in pdf_buffers:
+            merger.append(buf)
+        merger.write(output)
+        merger.close()
+        output.seek(0)
+        
+        return send_file(output, as_attachment=True, download_name='programacion_vuelos_qt.pdf', mimetype='application/pdf')
     return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
